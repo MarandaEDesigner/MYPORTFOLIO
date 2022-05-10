0f70e4b12d1f6fe67745f5230371c47429835a7d
MarandaEDesigner <MarandaEDesigner@gmail.com>
2022-05-10 12:45:52 -0400

    cobe
    cobreeee

--- functions.php
+++ functions.php
@@ -12,6 +12,9 @@ function enqueue_my_styles_and_scripts(){
   wp_enqueue_style('my-styles', get_stylesheet_directory_uri() . '/css/styles.css',
   array(), '1.0.0', 'all');
 
+  wp_enqueue_style('gallery-styles', get_stylesheet_directory_uri() . '/css/gallery.css',
+  array(), '1.0.0', 'all');
+
   wp_enqueue_script('my-scripts', get_theme_file_uri() . '/js/gallery.js',
   array(), '1.0.0', true);
 
